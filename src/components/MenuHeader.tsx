0db7731a5317a768b6ffd2896c2a24f70282b5c1
Devmaxmonteiro <max.0208197795@educacao.pauloafonso.ba.gov.br>
2025-06-06 09:03:51 -0300

    feat: logo definitiva baseada na segunda imagem - semicírculo à direita do círculo

--- src/components/MenuHeader.tsx
+++ src/components/MenuHeader.tsx
@@ -15,7 +15,7 @@ export const MenuHeader: React.FC<MenuHeaderProps> = ({ restaurantInfo }) => {
       {/* Logo Section - Full Width */}
       <div className="w-full">
         <Image
-          src={`/images/tapioca-logo-nova.svg?cache=${timestamp}`}
+          src={`/images/tapioca-logo-definitiva.svg?cache=${timestamp}`}
           alt="Logo Tapioca Recheada de Chapa"
           width={800}
           height={300}
